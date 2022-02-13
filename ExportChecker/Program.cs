7a9fa99b8fd584f89d4e9d21f2e737ea028bbc15
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-13 17:57:47 -0500

    Add project files.

---
+++ ExportChecker/Program.cs
@@ -0,0 +1,52 @@
+﻿using AsmResolver.PE;
+using System;
+
+namespace ExportChecker
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			if(args.Length != 1)
+				Console.WriteLine("This program takes exactly one argument: the path to the pe assembly");
+
+			try
+			{
+				Run(args[0]);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+
+			Console.ReadLine();
+		}
+
+		private static void Run(string path)
+		{
+			IPEImage peImage = PEImage.FromFile(path);
+			if(peImage.Exports == null)
+			{
+				Console.WriteLine("File has no export directory");
+			}
+			else if (peImage.Exports.Entries.Count == 0)
+			{
+				Console.WriteLine("File has 0 export methods");
+			}
+			else
+			{
+				Console.WriteLine($"File has {peImage.Exports.Entries.Count} export methods");
+				Console.WriteLine();
+				foreach (var symbol in peImage.Exports.Entries)
+				{
+					Console.WriteLine($"Ordinal: {symbol.Ordinal}");
+					if (symbol.IsByName)
+						Console.WriteLine($"\tName: {symbol.Name}");
+					Console.WriteLine($"\tAddress: {symbol.Address.Rva.ToString("X8")}");
+				}
+				Console.WriteLine();
+				Console.WriteLine($"File had {peImage.Exports.Entries.Count} export methods");
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# ExportChecker
+
+A simple commandline application to list all the exports of a PE binary file using [AsmResolver](https://github.com/Washi1337/AsmResolver).
