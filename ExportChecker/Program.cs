4d6e2878cedf00ee566254bea29e6eae481da1c3
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-01-01 17:47:46 -0500

    file scoped namespace

--- ExportChecker/Program.cs
+++ ExportChecker/Program.cs
@@ -1,52 +1,51 @@
 ﻿using AsmResolver.PE;
 using System;
 
-namespace ExportChecker
+namespace ExportChecker;
+
+internal class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		if(args.Length != 1)
+			Console.WriteLine("This program takes exactly one argument: the path to the pe assembly");
+
+		try
 		{
-			if(args.Length != 1)
-				Console.WriteLine("This program takes exactly one argument: the path to the pe assembly");
-
-			try
-			{
-				Run(args[0]);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.ToString());
-			}
-
-			Console.ReadLine();
+			Run(args[0]);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.ToString());
 		}
 
-		private static void Run(string path)
+		Console.ReadLine();
+	}
+
+	private static void Run(string path)
+	{
+		PEImage peImage = PEImage.FromFile(path);
+		if(peImage.Exports == null)
 		{
-			PEImage peImage = PEImage.FromFile(path);
-			if(peImage.Exports == null)
+			Console.WriteLine("File has no export directory");
+		}
+		else if (peImage.Exports.Entries.Count == 0)
+		{
+			Console.WriteLine("File has 0 export methods");
+		}
+		else
+		{
+			Console.WriteLine($"File has {peImage.Exports.Entries.Count} export methods");
+			Console.WriteLine();
+			foreach (var symbol in peImage.Exports.Entries)
 			{
-				Console.WriteLine("File has no export directory");
+				Console.WriteLine($"Ordinal: {symbol.Ordinal}");
+				if (symbol.IsByName)
+					Console.WriteLine($"\tName: {symbol.Name}");
+				Console.WriteLine($"\tAddress: {symbol.Address.Rva.ToString("X8")}");
 			}
-			else if (peImage.Exports.Entries.Count == 0)
-			{
-				Console.WriteLine("File has 0 export methods");
-			}
-			else
-			{
-				Console.WriteLine($"File has {peImage.Exports.Entries.Count} export methods");
-				Console.WriteLine();
-				foreach (var symbol in peImage.Exports.Entries)
-				{
-					Console.WriteLine($"Ordinal: {symbol.Ordinal}");
-					if (symbol.IsByName)
-						Console.WriteLine($"\tName: {symbol.Name}");
-					Console.WriteLine($"\tAddress: {symbol.Address.Rva.ToString("X8")}");
-				}
-				Console.WriteLine();
-				Console.WriteLine($"File had {peImage.Exports.Entries.Count} export methods");
-			}
+			Console.WriteLine();
+			Console.WriteLine($"File had {peImage.Exports.Entries.Count} export methods");
 		}
 	}
 }
--- ImportChecker/Program.cs
+++ ImportChecker/Program.cs
@@ -2,58 +2,57 @@
 using AsmResolver.PE.Imports;
 using System;
 
-namespace ImportChecker
+namespace ImportChecker;
+
+internal class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		if(args.Length != 1)
+			Console.WriteLine("This program takes exactly one argument: the path to the pe assembly");
+
+		try
 		{
-			if(args.Length != 1)
-				Console.WriteLine("This program takes exactly one argument: the path to the pe assembly");
-
-			try
-			{
-				Run(args[0]);
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.ToString());
-			}
-
-			Console.ReadLine();
+			Run(args[0]);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.ToString());
 		}
 
-		private static void Run(string path)
+		Console.ReadLine();
+	}
+
+	private static void Run(string path)
+	{
+		PEImage peImage = PEImage.FromFile(path);
+		if(peImage.Imports == null)
 		{
-			PEImage peImage = PEImage.FromFile(path);
-			if(peImage.Imports == null)
-			{
-				Console.WriteLine("File has no import directory");
-				return;
-			}
-			Console.WriteLine($"File has {peImage.Imports.Count} imported modules");
-			Console.WriteLine();
-			for (int i = 0; i < peImage.Imports.Count; i++)
-			{
-				Console.WriteLine($"Module {i + 1}");
-				ImportedModule module = peImage.Imports[i];
-				Console.WriteLine($"\tName: {module.Name}");
-				Console.WriteLine($"\tForwarderChain: {module.ForwarderChain}");
-				Console.WriteLine($"\tTimeDateStamp: {module.TimeDateStamp}");
-				Console.WriteLine($"\tSymbols: {module.Symbols.Count}");
-				for(int j = 0; j < module.Symbols.Count; j++)
-				{
-					ImportedSymbol symbol = module.Symbols[j];
-					Console.WriteLine($"\t\tOrdinal: {symbol.Ordinal}");
-					Console.WriteLine($"\t\tName: {symbol.Name}");
-					if(symbol.Ordinal != symbol.Hint)
-						Console.WriteLine($"\t\tHint: {symbol.Hint}");
-					Console.WriteLine($"\t\tAddress: {symbol.AddressTableEntry?.Rva.ToString("X8")}");
-					Console.WriteLine();
-				}
-			}
-			Console.WriteLine();
-			Console.WriteLine($"File has {peImage.Imports.Count} imported modules");
+			Console.WriteLine("File has no import directory");
+			return;
 		}
+		Console.WriteLine($"File has {peImage.Imports.Count} imported modules");
+		Console.WriteLine();
+		for (int i = 0; i < peImage.Imports.Count; i++)
+		{
+			Console.WriteLine($"Module {i + 1}");
+			ImportedModule module = peImage.Imports[i];
+			Console.WriteLine($"\tName: {module.Name}");
+			Console.WriteLine($"\tForwarderChain: {module.ForwarderChain}");
+			Console.WriteLine($"\tTimeDateStamp: {module.TimeDateStamp}");
+			Console.WriteLine($"\tSymbols: {module.Symbols.Count}");
+			for(int j = 0; j < module.Symbols.Count; j++)
+			{
+				ImportedSymbol symbol = module.Symbols[j];
+				Console.WriteLine($"\t\tOrdinal: {symbol.Ordinal}");
+				Console.WriteLine($"\t\tName: {symbol.Name}");
+				if(symbol.Ordinal != symbol.Hint)
+					Console.WriteLine($"\t\tHint: {symbol.Hint}");
+				Console.WriteLine($"\t\tAddress: {symbol.AddressTableEntry?.Rva.ToString("X8")}");
+				Console.WriteLine();
+			}
+		}
+		Console.WriteLine();
+		Console.WriteLine($"File has {peImage.Imports.Count} imported modules");
 	}
 }
